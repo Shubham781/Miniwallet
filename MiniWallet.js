f17997bf4703c089525a44e99b53b8240d9ff6f3
Shubham781 <164032422+Shubham781@users.noreply.github.com>
2024-04-03 17:10:25 +0530

    Update MiniWallet.js

--- MiniWallet.js
+++ MiniWallet.js
@@ -9,15 +9,15 @@ const { Web3 } = require('web3');
 // const Web3 = require('web3');
 
 const apikey = process.env['apikey'];
-const network = 'holesky';
+const network = 'holesky'; //we have been used 'holesky' network with etherium protocol, u can use any network of your choice.
 const node = `https://go.getblock.io/${apikey}/${network}/`;
 
 const web3 = new Web3(node);
 
-// console.log(web3);
+// console.log(web3); <- to check if Web3 working or not
 
 const accountTo = web3.eth.accounts.create();
-// console.log(accountTo);
+// console.log(accountTo); <- to display information about new account that is created
 
 const privatekey = process.env['privatekey'];
 const accountFrom = web3.eth.accounts.privateKeyToAccount(privatekey);
@@ -28,9 +28,6 @@ const createSignedTx = async(rawTx)=>{
     return await accountFrom.signTransaction(rawTx);
 }
 
-// const sendSignedTx = async(signedTx)={
-//      web3.eth.sendSignedTransaction(signedTx.rawTransaction).then(console.log);
-// }
 const sendSignedTx = async (signedTx) => {
   return web3.eth.sendSignedTransaction(signedTx.rawTransaction);
 }; 
